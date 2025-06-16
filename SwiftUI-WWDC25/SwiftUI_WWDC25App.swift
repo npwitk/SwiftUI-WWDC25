54047f0038e17f334b729b0d11ef81909f53779e
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-06-16 22:39:36 +0700

    Initial Commit

---
+++ SwiftUI-WWDC25/SwiftUI_WWDC25App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WWDC25App.swift
+//  SwiftUI-WWDC25
+//
+//  Created by Nonprawich I. on 16/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WWDC25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
